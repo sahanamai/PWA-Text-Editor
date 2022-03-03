5e24cd9ad98bcb331c8e7780b5ba9c0b9a0379ba
sahana <sahanamai@gmail.com>
2022-03-02 18:51:26 -0600

    putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,11 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id,content) => {
   console.error('putDb not implemented');
 // Create a connection to the database database and version we want to use.
-const contactDb = await openDB('contact', 1);
-
+ const editorDb = await openDB('jate', 1);
+const tx = editorDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, 'jate': content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
 
 };
 
